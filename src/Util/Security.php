cc6e62b9c383dfd9697807224b12f9ebfe3af409
clmntgbr <clement.goubier@gmail.com>
2019-07-10 17:49:14 +0200

    adding Security util

--- src/Util/Security.php
+++ src/Util/Security.php
@@ -9,6 +9,9 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class Security
 {
@@ -27,13 +30,81 @@ class Security
     /* @var EntityManagerInterface */
     private $em;
 
-    public function __construct(EncoderFactoryInterface $encoder, UserPasswordEncoderInterface $passwordEncoder, TokenStorageInterface $tokenStorage, SessionInterface $session, EntityManagerInterface $em)
+    /* @var ValidatorInterface */
+    private $validator;
+
+    public function __construct(EncoderFactoryInterface $encoder, UserPasswordEncoderInterface $passwordEncoder, TokenStorageInterface $tokenStorage, SessionInterface $session, EntityManagerInterface $em, ValidatorInterface $validator)
     {
         $this->encoder = $encoder;
         $this->passwordEncoder = $passwordEncoder;
         $this->tokenStorage = $tokenStorage;
         $this->session = $session;
         $this->em = $em;
+        $this->validator = $validator;
+    }
+
+    public function login($email, $password)
+    {
+        $constraints = [
+            new Email(),
+            new NotBlank()
+        ];
+
+        /* @var User */
+        $user = new User();
+
+        $result = false;
+
+        if ($email !== null && $password !== null) {
+            $errors = $this->validator->validate($email, $constraints);
+            if (count($errors) > 0) {
+                return ["status" => false, "message" => "$email is not a valid email address."];
+            } else {
+                $user = $this->em->getRepository(User::class)->findOneBy(["email" => $email]);
+                if ($user instanceof User) {
+                    $encoder = $this->encoder->getEncoder($user);
+                    $result = $encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt());
+                }
+            }
+        }
+
+        if ($result) {
+            $this->setToken($user);
+            $user->setLastLogin(new \DateTime());
+            $this->em->persist($user);
+            $this->em->flush();
+            return ["status" => true];
+        }
+
+        return ["status" => false, "message" => "Email or/and password invalid."];
+    }
+
+    public function register($email, $password, $passwordConfirmation, $role = 'ROLE_USER', $tokenIt = true)
+    {
+        $errors = $this->validator->validate($email, [new Email(), new NotBlank()]);
+        if (count($errors) > 0) {
+            return ["status" => false, "message" => "$email is not a valid email address."];
+        }
+
+        $result = $this->em->getRepository(User::class)->findOneBy(['email' => $email]);
+        if ($result) {
+            return ["status" => false, "message" => "This $email address already exist."];
+        }
+
+        $errors = $this->validator->validate($password, [new NotBlank()]);
+        if (count($errors) > 0) {
+            return ["status" => false, "message" => "Your password is empty."];
+        }
+        if ($password == $passwordConfirmation) {
+            $user = $this->createNewUser($email, $password, $role);
+            if ($tokenIt) {
+                $this->setToken($user);
+            }
+            $this->em->persist($user);
+            $this->em->flush();
+            return ["status" => true, "message" => "User '$email' have been created."];
+        }
+        return ["status" => false, "message" => "Your passwords must correspond."];
     }
 
     public function changePassword(User $entity, string $oldPassword, string $newPassword)
